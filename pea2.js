61cd1bd1db82442f2edcfa3b7bbfba58258e3077
darkLuciferDevilOfHisWord <86288159+darkLuciferDevilOfHisWord@users.noreply.github.com>
2021-12-19 15:05:00 +0530

    Add files via upload

---
+++ pea2.js
@@ -0,0 +1,220 @@
+
+let img1_1 = ["pic","pic"];
+let img1_2 = [];
+let img1_3 = [];
+
+function fun1_1(){
+    img1_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun1_2(){
+    img1_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun1_3(){
+    img1_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+let img2_1 = [];
+let img2_2 = [];
+let img2_3 = [];
+
+function fun2_1(){
+    img2_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun2_2(){
+    img2_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun2_3(){
+    img2_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+let img3_1 = [];
+let img3_2 = [];
+let img3_3 = [];
+
+function fun3_1(){
+    img3_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun3_2(){
+    img3_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun3_3(){
+    img3_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+let img4_1 = [];
+let img4_2 = [];
+let img4_3 = [];
+
+function fun4_1(){
+    img4_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun4_2(){
+    img4_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun4_3(){
+    img4_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+let img5_1 = [];
+let img5_2 = [];
+let img5_3 = [];
+
+function fun5_1(){
+    img5_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun5_2(){
+    img5_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun5_3(){
+    img5_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+
+
+
+
+let img6_1 = [];
+let img6_2 = [];
+let img6_3 = [];
+
+function fun6_1(){
+    img6_1.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun6_2(){
+    img6_2.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
+
+function fun6_3(){
+    img6_3.map((img)=>{
+        mainBox.innerHTML += `
+        <hr>
+        <img src="${img}.jpg" alt="" class="img-fluid">
+        `;
+    })
+    mainBox.innerHTML += `<hr>`;
+}
